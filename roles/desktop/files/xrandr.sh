8910a55828b1fb6e9de789108c26c5c63a539dc7
Dylan Meissner <dylanmei@gmail.com>
2016-04-22 07:30:24 -0700

    Fix resolution when using DPI1

--- roles/desktop/files/xrandr.sh
+++ roles/desktop/files/xrandr.sh
@@ -5,8 +5,8 @@ external="DP1"
 hdmi="HDMI1"
 
 if (xrandr | grep -e "^$external connected"); then
-  xrandr --output $laptop --primary --mode 2560x1440 --rotate normal \
-         --output $external --above $laptop --mode 1920x1080 --rotate normal \
+  xrandr --output $laptop --primary --mode 1920x1080 --rotate normal \
+         --output $external --above $laptop --mode 2560x1440 --rotate normal \
          --output $hdmi --off \
          --output VIRTUAL1 --off
 elif (xrandr | grep -e "^$hdmi connected"); then
